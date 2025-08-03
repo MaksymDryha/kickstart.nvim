7a9ccf53b050ad18d5967c74a6df69e73287673d
Maksym Dryha <max.dryha@gmail.com>
2025-08-03 23:40:16 +0300

    fix gitsigns mappings

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -1,12 +1,8 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup {
-      config = function()
+    'lewis6991/gitsigns.nvim',
+    config = function()
         vim.keymap.set('n', '<leader>gb', ':Gitsigns blame<CR>')
         vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk_inline<CR>')
         vim.keymap.set('n', '<leader>gu', ':Gitsigns reset_hunk<CR>')
-      end
-    }
-  end,
+    end
 }
