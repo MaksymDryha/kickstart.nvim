28263a05c08574d19d72f8a5176d80a7e41a8be8
Maksym Dryha <max.dryha@gmail.com>
2025-08-04 00:04:11 +0300

    improve gitsigns chars

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -4,5 +4,19 @@ return {
         vim.keymap.set('n', '<leader>gb', ':Gitsigns blame<CR>')
         vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk_inline<CR>')
         vim.keymap.set('n', '<leader>gu', ':Gitsigns reset_hunk<CR>')
+
+        require("gitsigns").setup({
+            signs = {
+                delete = {
+                    text = '▶'
+                },
+                topdelete = {
+                    text = '▶'
+                },
+                changedelete = {
+                    text = '▶'
+                }
+            }
+        });
     end
 }
