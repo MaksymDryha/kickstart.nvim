448453b327832a87eff53c6dd0ca8d5d21ae7655
Max Dryha <max.dryha@gmail.com>
2025-08-03 19:15:30 +0300

    gitsings shortcuts

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -2,8 +2,11 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup {
-      vim.keymap.set('n', '<leader>gb', ':Gitsigns blame<CR>'),
-      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk_inline<CR>')
+      config = function()
+        vim.keymap.set('n', '<leader>gb', ':Gitsigns blame<CR>')
+        vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk_inline<CR>')
+        vim.keymap.set('n', '<leader>gu', ':Gitsigns reset_hunk<CR>')
+      end
     }
   end,
 }
