8e40109bdea0fd92965e552299f8bf5900c62013
Maksym Dryha <max.dryha@gmail.com>
2025-08-04 21:22:59 +0300

    best color scheme in the universe

--- lua/plugins/configs/color-scheme.lua
+++ lua/plugins/configs/color-scheme.lua
@@ -1,8 +1,7 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-        vim.cmd.colorscheme "catppuccin-macchiato"
+        vim.cmd("colorscheme rose-pine")
     end
 }
