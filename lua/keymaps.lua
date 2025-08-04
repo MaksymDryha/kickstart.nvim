a24f69ff041b110f1a40653605c4a911a15ae848
Maksym Dryha <max.dryha@gmail.com>
2025-08-04 21:35:20 +0300

    window management keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,13 +2,17 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfix list' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {
+    desc = 'Open diagnostic Quickfix list'
+})
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', {
+    desc = 'Exit terminal mode'
+})
 
 -- Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '')
@@ -17,9 +21,38 @@ vim.keymap.set('n', '<up>', '')
 vim.keymap.set('n', '<down>', '')
 
 -- faster scrolling
-vim.keymap.set("n", "<C-j>", "10j", { noremap = true })
-vim.keymap.set("n", "<C-k>", "10k", { noremap = true })
+vim.keymap.set("n", "<C-j>", "10j", {
+    noremap = true
+})
+vim.keymap.set("n", "<C-k>", "10k", {
+    noremap = true
+})
 
 vim.keymap.set('n', 'K', function()
-  vim.lsp.buf.hover({ border = 'single' })
+    vim.lsp.buf.hover({
+        border = 'single'
+    })
 end)
+
+-- window management
+-- helper
+local map = vim.keymap.set
+local opts = {
+    noremap = true,
+    silent = true
+}
+
+-- Move to other windows
+map("n", "<leader>wh", "<C-w>h", opts)
+map("n", "<leader>wj", "<C-w>j", opts)
+map("n", "<leader>wk", "<C-w>k", opts)
+map("n", "<leader>wl", "<C-w>l", opts)
+
+-- Toggle between windows (cycles through)
+map("n", "<leader>ww", "<C-w>w", opts)
+
+-- Close current window
+map("n", "<leader>wd", "<C-w>c", opts)
+
+map("n", "<leader>wn", ":vsplit<CR>", opts)
+
