649e4d35a18ca251c97a5177079789ed963bf317
Maksym Dryha <max.dryha@gmail.com>
2025-08-14 22:35:27 +0300

    improve inline diagnostics formatting

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -140,8 +140,13 @@ return {
             end
         })
 
-        -- Diagnostic Config
-        -- See :help vim.diagnostic.Opts
+        local inlineDiagnosticsSigns = {
+            ERROR = ' ',
+            WARN = ' ',
+            HINT = ' 󰌵',
+            INFO = ' '
+        }
+
         vim.diagnostic.config {
             severity_sort = true,
             float = {
@@ -162,12 +167,15 @@ return {
             virtual_text = {
                 source = 'if_many',
                 spacing = 2,
+                prefix = function(diagnostic)
+                    return inlineDiagnosticsSigns[vim.diagnostic.severity[diagnostic.severity]]
+                end,
                 format = function(diagnostic)
                     local diagnostic_message = {
-                        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                        [vim.diagnostic.severity.WARN] = diagnostic.message,
-                        [vim.diagnostic.severity.INFO] = diagnostic.message,
-                        [vim.diagnostic.severity.HINT] = diagnostic.message
+                        [vim.diagnostic.severity.ERROR] = diagnostic.message .. ' ',
+                        [vim.diagnostic.severity.WARN] = diagnostic.message .. ' ',
+                        [vim.diagnostic.severity.INFO] = diagnostic.message .. ' ',
+                        [vim.diagnostic.severity.HINT] = diagnostic.message .. ' '
                     }
                     return diagnostic_message[diagnostic.severity]
                 end
